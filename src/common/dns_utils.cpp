2cfee64a460f67140dd3a4af474f91321f0bbe38
Yudong Li <liyudong1@hotmail.com>
2018-03-27 20:39:49 -0500

    nessesary name change

--- src/common/dns_utils.cpp
+++ src/common/dns_utils.cpp
@@ -345,7 +345,7 @@ namespace dns_utils
 std::string address_from_txt_record(const std::string& s)
 {
   // make sure the txt record has "oa1:xmr" and find it
-  auto pos = s.find("oa1:aeon");
+  auto pos = s.find("oa1:aureum");
   if (pos == std::string::npos)
     return {};
   // search from there to find "recipient_address="
@@ -505,7 +505,7 @@ bool load_txt_records_from_dns(std::vector<std::string> &good_records, const std
 
   if (num_valid_records < 2)
   {
-    LOG_PRINT_L0("WARNING: no two valid MoneroPulse DNS checkpoint records were received");
+    LOG_PRINT_L0("WARNING: no two valid AureumPulse DNS checkpoint records were received");
     return false;
   }
 
@@ -527,7 +527,7 @@ bool load_txt_records_from_dns(std::vector<std::string> &good_records, const std
 
   if (good_records_index < 0)
   {
-    LOG_PRINT_L0("WARNING: no two MoneroPulse DNS checkpoint records matched");
+    LOG_PRINT_L0("WARNING: no two AureumPulse DNS checkpoint records matched");
     return false;
   }
 
